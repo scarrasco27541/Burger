341bcb9754576414ccea49e161881b3714910bc0
Sherry Carrasco <sc27541@gmail.com>
2019-03-23 17:57:34 -0400

    got routes working with get, update, add burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,7 @@
-//burger = require("../models/burger");
+const path = require("path");
+burger = require("../models/burger");
 
+/*
 module.exports = function(app) {
 	app.get("/", function(req, res) {
 		res.send("All our burgers!");
@@ -16,4 +18,32 @@ module.exports = function(app) {
 //	app.post("/burger/eat/:burger_id", function(req, res) {
 //		res.send("Eat a burger: " + req.body.burger_id);
 //	});
+};
+*/
+
+
+
+module.exports = function(app) {
+	app.get("/", function(req, res) {
+		burger.getBurgers(function(burgers) {
+			res.render(path.join(__dirname, "../views/index"), {burgers:burgers});
+		});
+	});
+	app.get("/burger/add/:burger_name", function(req, res) {
+		burger.addBurger(req.params.burger_name, function(burgers) {
+			res.redirect('/');
+		});
+	});
+	app.get("/burger/eat/:burger_id", function(req, res) {
+		burger.devourBurger(req.params.burger_id, function(burgers) {
+			burger.getBurgers(function(burgers) {
+				res.json(burgers);
+			});
+		});
+	});
+	app.post("/burger/add", function(req, res) {
+		burger.addBurger(req.body.burger_name, function(burgers) {
+			res.redirect('/');
+		});
+	});
 };
--- server.js
+++ server.js
@@ -8,6 +8,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require("./controllers/burgers_controller")(app);
 
+// ---
+// HELP FROM:
+// https://stackoverflow.com/questions/43704187/cant-get-express-handlebars-render-an-html-page
+// https://hackersandslackers.com/handlebars-templating-in-expressjs/
+// ---
+var hbs = require( 'express-handlebars');
+app.engine('handlebars', hbs({
+  defaultLayout: 'main',
+  extname: 'handlebars',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+// ---
+
+// https://expressjs.com/en/starter/static-files.html
+// This line sets 'public' as a static location that routes the 'normal' way
+//app.use(express.static('public'))
+// ---
+
+
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
